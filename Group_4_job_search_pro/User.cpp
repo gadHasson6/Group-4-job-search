1b152896d0f585fd629c361f9f9f1436a03af2b1
yuvalhazut7 <159694488+yuvalhazut7@users.noreply.github.com>
2024-02-25 12:16:41 +0200

    first

--- Group_4_job_search_pro/User.cpp
+++ Group_4_job_search_pro/User.cpp
@@ -1,5 +1,2 @@
-//
-// Created by LENOVO on 21/02/2024.
-//
 
 #include "User.h"
--- Group_4_job_search_pro/User.h
+++ Group_4_job_search_pro/User.h
@@ -1,7 +1,3 @@
-//
-// Created by LENOVO on 21/02/2024.
-//
-
 #ifndef GROUP_4_JOB_SEARCH_PRO_USER_H
 #define GROUP_4_JOB_SEARCH_PRO_USER_H
 
