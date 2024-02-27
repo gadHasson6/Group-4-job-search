dcafa002395d615be2b6e1e0797f38c38ea96b6a
gadHasson6 <gadi.hasson6@gmail.com>
2024-02-27 19:45:24 +0200

    1234

--- Group_4_job_search_pro/main.cpp
+++ Group_4_job_search_pro/main.cpp
@@ -7,12 +7,21 @@ using namespace std;
 
 
 int main() {
-    const char  * fff = DATA_BASE_PATH;
-    sqlite3 * db = openSQLiteFile(fff);
-    cout <<"employers: " << countRowsInEmployersTable(db) << '\n';
-    cout <<"candidates: " << countRowsInCandidatesTable(db) << '\n';
-    cout <<"employer 12 have : " << countRowsByCreatorID(db, 12) << " jobs\n";
-    cout <<"employer 14 have : " << countRowsByCreatorID(db, 14) << " jobs\n";
-    closeSQLiteFile(db);
+    int x =0, y= 2, z=3;
+    int * aa[3];
+    aa[0] = &x;
+    aa[1] = &y;
+    aa[2] = &z;
+    x=5;
+    *aa[1] = 9;
+    cout << "x=" << *aa[0] << "y = " << *aa[1] << "\n";
+    cout << "x=" << x << "y = " << y << "\n";
+//    const char  * fff = DATA_BASE_PATH;
+//    sqlite3 * db = openSQLiteFile(fff);
+//    cout <<"employers: " << countRowsInEmployersTable(db) << '\n';
+//    cout <<"candidates: " << countRowsInCandidatesTable(db) << '\n';
+//    cout <<"employer 12 have : " << countRowsByCreatorID(db, 12) << " jobs\n";
+//    cout <<"employer 14 have : " << countRowsByCreatorID(db, 14) << " jobs\n";
+//    closeSQLiteFile(db);
     return 0;
 }
