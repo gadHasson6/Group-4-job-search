26cda2278ed109d4a49058ca3464c005e7edc415
avigail24 <avigailbenitta@gmail.com>
2024-02-27 19:42:08 +0200

    vbv

--- Group_4_job_search_pro/Employer.h
+++ Group_4_job_search_pro/Employer.h
@@ -4,9 +4,21 @@
 
 #ifndef GROUP_4_JOB_SEARCH_PRO_EMPLOYER_H
 #define GROUP_4_JOB_SEARCH_PRO_EMPLOYER_H
+using namespace std;
+#include "Job.h"
 
 
 class Employer {
+private:
+    Job* my_job;
+public:
+    void set(Job*item){my_job=item;}
+    Employer();
+    Employer(long id, string full_name,string mail,long phone_num,string paswoord)
+    void edit_job();
+    void publich_job();
+    void view_job();
+
 
 };
 
