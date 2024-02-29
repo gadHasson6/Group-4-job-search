d49e4aaee2165be90bd71dd24472ce30821e5f61
Shifra Avigdor <121171039+shifraa@users.noreply.github.com>
2024-02-29 19:11:42 +0200

    shifra

--- Group_4_job_search_pro/Apply.h
+++ Group_4_job_search_pro/Apply.h
@@ -1,12 +1,29 @@
 //
 // Created by LENOVO on 21/02/2024.
 //
-
+include <Date.h>
 #ifndef GROUP_4_JOB_SEARCH_PRO_APPLY_H
 #define GROUP_4_JOB_SEARCH_PRO_APPLY_H
 
 
 class Apply {
+
+private:
+
+    //THE FIELDS OF APPLY CLASS:
+    long numberOfSubmission;
+    long candidateID;
+    long jobID;
+    job* job;
+    candidate* candidate;
+    date dateOfSubmission;
+    string statusOfSubmission;
+
+public:
+
+    Apply();
+    Apply(long numberOfSubmission , long candidateID , long jobI , job* job , candidate* candidate , date dateOfSubmission ,string statusOfSubmission);
+
 
 };
 
