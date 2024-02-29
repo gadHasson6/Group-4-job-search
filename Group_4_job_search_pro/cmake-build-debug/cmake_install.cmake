c93a54bb5befabb59c9ca36b4baa75669a394fe9
gadHasson6 <gadi.hasson6@gmail.com>
2024-02-29 18:57:15 +0200

    step #3 in connecting the classes that were written separately

--- Group_4_job_search_pro/cmake-build-debug/cmake_install.cmake
+++ Group_4_job_search_pro/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ariel/CLionProjects/job_search/Group-4-job-search-F/Group_4_job_search_pro
+# Install script for directory: C:/Users/LENOVO/programing_SWing/Group-4-job-search/Group_4_job_search_pro
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.3.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.3.3/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ariel/CLionProjects/job_search/Group-4-job-search-F/Group_4_job_search_pro/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/LENOVO/programing_SWing/Group-4-job-search/Group_4_job_search_pro/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
