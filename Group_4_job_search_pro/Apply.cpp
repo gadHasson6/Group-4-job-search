f47a993201a8719d833f3670207c93933b3f2d3a
Shifra Avigdor <121171039+shifraa@users.noreply.github.com>
2024-02-29 20:22:51 +0200

    shifra

--- Group_4_job_search_pro/Apply.cpp
+++ Group_4_job_search_pro/Apply.cpp
@@ -1,5 +1,38 @@
-//
+    //Reminder of Apply the fields:
+
+//long numberOfSubmission;
+//    long candidateID;
+//    long jobID;
+//    job* job;
+//    candidate* candidate;
+//    date dateOfSubmission;
+//    string statusOfSubmission;
 // Created by LENOVO on 21/02/2024.
 //
 
 #include "Apply.h"
+
+
+Aply::Apply(){
+
+    numberOfSubmission = 0;
+    candidateID = 000000000;
+    jobID = 000000000;
+    job =  nullptr;
+    candidate =  nullptr;;
+    dateOfSubmission = 0 ;
+    string statusOfSubmission = "none";
+
+}
+
+Apply::Apply(long numberOfSubmission , long candidateID , long jobI , job* job , candidate* candidate , date dateOfSubmission ,string statusOfSubmission){
+
+    this -> numberOfSubmission = numberOfSubmission;
+    this -> candidateID = candidateID;
+    this -> jobI = jobI;
+    this -> job = &job ;
+    this -> candidate = &candidate ;
+    this ->  dateOfSubmission  = dateOfSubmission;
+    this -> statusOfSubmission = statusOfSubmission;
+
+}
