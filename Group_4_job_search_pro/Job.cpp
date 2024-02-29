dbd1734137252bf5a4d48f96eea588ef014677e8
keojeN <vladir9@gmail.com>
2024-03-01 01:57:44 +0200

    Update #4

--- Group_4_job_search_pro/Job.cpp
+++ Group_4_job_search_pro/Job.cpp
@@ -2,7 +2,21 @@
 // Created by LENOVO on 21/02/2024.
 //
 #include "Job.h"
-Job::Job(const string& occuption,const string& company_name,const string& company_location,const string& company_number,const string& requirements,const Job_scope& scope ,const int& experience){
+Job::Job() {
+    m_posting_status=false;
+    m_Applies=null;
+    m_applies_size=0;
+    m_job_id=0;
+    m_occupation="Unknown";
+    m_company_name="Unknown";
+    m_copmany_location="Unknown";
+    m_phone_number="Unknown";
+    m_requirements="Unknown";
+    m_scope=0;
+    m_experience_needed=0;
+}
+Job::Job(const int& job_id ,const string& occuption,const string& company_name,const string& company_location,const string& company_number,const string& requirements,const Job_scope& scope ,const int& experience){
+    m_job_id=job_id;
     m_occupation=occuption;
     m_company_name=company_name;
     m_copmany_location=company_location;
@@ -13,6 +27,8 @@ Job::Job(const string& occuption,const string& company_name,const string& compan
 }
 
 void Job::print_job() const{
+
+    cout << "Job id: " << m_job_id << endl;
   cout << "Jobs Occupation: " << m_occupation << endl;
   cout << "Company name: " << m_company_name << endl;
   cout << "Company location: " << m_copmany_location << endl;
@@ -23,4 +39,48 @@ void Job::print_job() const{
       cout << "Part time job" << endl;
   cout << "Requirements: " << m_requirements << endl;
   cout << m_experience_needed << " of experience needed!" << endl;
+}
+void Job::set_applies(Apply** appliesArray, int size) {
+    // Release previously allocated memory
+    if (m_Applies != nullptr) {
+        for (int i = 0; i < m_applies_size; ++i) {
+            delete m_Applies[i];
+        }
+        delete[] m_Applies;
+    }
+
+    // Allocate new memory and copy the applies
+    m_applies_size = size;
+    m_Applies = new Apply*[size];
+    for (int i = 0; i < size; ++i) {
+        m_Applies[i] = appliesArray[i];
+    }
+}
+
+Job::~Job(){
+    if (m_Applies != nullptr) {
+        for (int i = 0; i < m_applies_size; ++i) {
+            delete m_Applies[i];
+        }
+        delete[] m_Applies;
+    }
+}
+
+void Job::Add_Apply(Apply* apply) {
+    m_applies_size++;
+    Apply** temp = new Apply*[m_applies_size];
+
+    // Copy existing applies to temp array
+    for(int i = 0; i < m_applies_size - 1; i++) {
+        temp[i] = m_Applies[i];
+    }
+
+    // Add the new apply
+    temp[m_applies_size - 1] = apply;
+
+    // Delete the old array and update with the new one
+    delete[] m_Applies;
+    m_Applies = temp;
+
+    cout << "Apply added successfully" << endl;
 }
--- Group_4_job_search_pro/Job.h
+++ Group_4_job_search_pro/Job.h
@@ -3,6 +3,7 @@
 //
 #include <cstring>
 #include "iostream"
+#include "Apply.h"
 #ifndef GROUP_4_JOB_SEARCH_PRO_JOB_H
 #define GROUP_4_JOB_SEARCH_PRO_JOB_H
 enum Job_scope {full_time = 0,part_time};
@@ -12,8 +13,8 @@ class Job {
 public:
     Job();
     ~Job();
-    Job(const string& occuption,const string& company_name,const string& company_location,const string& company_number,const string& requirements,const Job_scope& scope,const int& experience);
-    Job(const Job& other) : m_occupation(other.m_occupation) ,m_company_name(other.m_company_name) ,m_copmany_location(other.m_copmany_location) , m_phone_number(other.m_phone_number),
+    Job(const int& job_id,const string& occuption,const string& company_name,const string& company_location,const string& company_number,const string& requirements,const Job_scope& scope,const int& experience);
+    Job(const Job& other) : m_job_id(other.m_job_id) , m_occupation(other.m_occupation) ,m_company_name(other.m_company_name) ,m_copmany_location(other.m_copmany_location) , m_phone_number(other.m_phone_number),
                             m_requirements(other.m_requirements) , m_scope(other.m_scope) , m_experience_needed(other.m_experience_needed) {};
     void print_job () const;
     void set_occupation(const string& occupation) { m_occupation = occupation; }
@@ -23,6 +24,8 @@ public:
     void set_requirements(const string& requirements) { m_requirements = requirements; }
     void set_scope(const Job_scope& scope) { m_scope = scope; }
     void set_experience(const int& experience) { m_experience_needed = experience; }
+    void set_applies(Apply** , int );
+    void set_status(const bool status) { m_posting_status=status; }
     const string& get_occupation() const { return m_occupation; }
     const string& get_name() const { return m_company_name; }
     const string& get_number() const { return m_phone_number; }
@@ -30,7 +33,12 @@ public:
     const string& get_requirements() const { return m_requirements; }
     const Job_scope& get_scope() const { return m_scope; }
     const int& get_experience() const { return m_experience_needed; }
+    const int& get_job_id() const { return m_job_id; }
+    const bool get_status () const {return m_posting_status; }
+
 private:
+    bool m_posting_status;
+    int m_job_id;
     string m_occupation;
     string m_company_name;
     string m_copmany_location;
@@ -38,6 +46,8 @@ private:
     string m_requirements;
     int m_experience_needed;
     Job_scope m_scope;
+    m_Apply** m_Applies;
+    int m_applies_size;
 };
 
 
