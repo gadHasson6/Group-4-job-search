71d36d16199ea3821838e5d9ce305d46a5bf88ae
avigail24 <avigailbenitta@gmail.com>
2024-02-29 19:06:08 +0200

    vbv

--- Group_4_job_search_pro/Employer.h
+++ Group_4_job_search_pro/Employer.h
@@ -15,7 +15,7 @@ private:
     Job* my_job;
 public:
     Employer(){my_job= nullptr;}
-    Employer(string full_name,lond id,string password,string email,long phone_num,string residence,int rating,string feedback );
+    Employer(string full_name,long id,string password,string email,long phone_num,string residence,int rating,string feedback );
     void set(Job*item);
     void E_Print_Job();
     void edit_job();
