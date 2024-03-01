044f5cba4d278d23e730c5ac10bc90ccb3ae5697
gadHasson6 <gadi.hasson6@gmail.com>
2024-03-01 21:00:30 +0200

    gad2 commit #4

--- Group_4_job_search_pro/Apply.cpp
+++ Group_4_job_search_pro/Apply.cpp
@@ -1,5 +1,157 @@
 //
 // Created by LENOVO on 21/02/2024.
 //
-
+/*
+Reminder : the fields of Apply class are:
+    1.long numberOfSubmission;
+    2.long candidateID;
+    3.long jobID;
+    4.Job* job;
+    5.Candidate* candidate;
+    6.Date submissionDate;
+    7.string submissionStatus
+*/
 #include "Apply.h"
+
+//Default constructor:
+Apply::Apply() {
+
+    this -> numberOfSubmission = 0;
+    this -> candidateID = 0;
+    this -> jobID = 0;
+    this -> job = nullptr;
+    this -> candidate = nullptr;
+    this->submissionDate = Date();
+    this -> submissionStatus = "no submissions found";
+
+}
+
+
+//Parameter constructor:
+//******************************* note: check is missing parameters for the submissionDate.Date() ******///
+
+Apply::Apply(long numberOfSubmission, long candidateID, long jobID,Job* job,
+             Candidate* candidate, Date submissionDate, string submissionStatus) {
+
+    this -> numberOfSubmission = numberOfSubmission;
+    this -> candidateID = candidateID;
+    this -> jobID = jobID;
+    this -> job = job;
+    this -> candidate = candidate;
+    this -> submissionDate = submissionDate.Date();
+    this -> submissionStatus = submissionStatus;
+
+}
+
+// Copy constructor:
+//******************************* note: check the copy constructor for deep copy *************************//
+
+Apply::Apply(const Apply& other)
+        : numberOfSubmission(other.numberOfSubmission),
+          candidateID(other.candidateID),
+          jobID(other.jobID),
+          job(nullptr),
+          candidate(nullptr),
+          submissionDate(other.submissionDate),
+          submissionStatus(other.submissionStatus) {
+
+}
+//  == operator :
+bool Apply::operator==(const Apply& other) const {
+    return (
+            numberOfSubmission == other.numberOfSubmission &&
+            candidateID == other.candidateID &&
+            jobID == other.jobID &&
+            ((job == nullptr && other.job == nullptr) || (*job == *(other.job))) &&
+            ((candidate == nullptr && other.candidate == nullptr) || (*candidate == *(other.candidate))) &&
+            submissionDate == other.submissionDate &&
+            submissionStatus == other.submissionStatus
+    );
+}
+
+
+
+// Print function for employer with candidateID:
+void Apply::printEmployer() const {
+    cout << "Employer Information:" << endl;
+    if (candidate) { //if candidate is not nullptr
+        cout << "Candidate ID: " << candidateID << endl;
+        cout << "Candidate Information:" << endl;
+        candidate->printThisCandidateInfo();
+    }
+    cout << "Submission Number: " << numberOfSubmission << endl;
+    cout << "Submission Date: " << this -> submissionDate.printDate() << endl;
+    cout << "Submission Status: " << submissionStatus << endl;
+/*  Reminder : the fields that should be printed are:
+                   1.numberOfSubmission;
+                   2.long candidateID;
+                   5.Candidate* candidate;
+                   6.submissionDate;
+                   7.submissionStatus                       */
+    cout << "These are all of the employer details." << endl;
+
+}
+
+// Print function for candidate (with jobID)
+void Apply::printCandidate() const {
+    cout << "Candidate Information:" << endl;
+    cout << "Number of Submission: " << numberOfSubmission << endl;
+    cout << "Job ID: " << jobID << endl;
+    // Call print_job method from Job class if job is not nullptr
+    if (job) {
+        job->print_job();
+    }
+    cout << "Submission Date: " << this -> submissionDate.printDate() << endl;
+    cout << "Submission Status: " << submissionStatus << endl;
+    /*  Reminder : the fields that should be printed are:
+                   1.numberOfSubmission;
+                   2.long candidateID;
+                   3.long jobID;
+                   4.Job* job;
+                   6.submissionDate;
+                   7.submissionStatus                       */
+    cout << "These are all of the candidate details." << endl;
+}
+
+
+//SET FUNCTIONS:
+void Apply::setNumberOfSubmission(long numberOfSubmission) {
+    this ->  numberOfSubmission = numberOfSubmission;}
+void Apply::setCandidateID(long candidateID) {
+    this ->  candidateID = candidateID;}
+void Apply::setJobID(long jobID) {
+    this ->  jobID = jobID;}
+void Apply::setJob(Job* job) {
+    this->job = job;}
+void Apply::setCandidate(Candidate* candidate) {
+    this->candidate = candidate;}
+void Apply::setSubmissionDate(const Date& submissionDate) {
+    this->submissionDate = submissionDate;}
+void Apply::setSubmissionStatus(const std::string& submissionStatus) {
+    this->submissionStatus = submissionStatus;}
+
+
+
+
+//GET FUNCTIONS:
+long Apply::getNumberOfSubmission() const {
+    return this -> numberOfSubmission;}
+long Apply::getCandidateID() const {
+    return this -> candidateID;}
+long Apply::getJobID() const {
+    return this -> jobID;}
+Date Apply::getSubmissionDate() const {
+    return this-> submissionDate;}
+const Job* Apply::getJob() const {
+    return this-> job;}
+const Candidate* Apply::getCandidate() const {
+    return this-> candidate;}
+string Apply::getSubmissionStatus() const {
+    return this-> submissionStatus;}
+
+//DESTRUCTOR
+Apply::~Apply() {
+    // Release dynamic memory if allocated
+    delete job;
+    delete candidate;
+}
--- Group_4_job_search_pro/Apply.h
+++ Group_4_job_search_pro/Apply.h
@@ -5,8 +5,61 @@
 #ifndef GROUP_4_JOB_SEARCH_PRO_APPLY_H
 #define GROUP_4_JOB_SEARCH_PRO_APPLY_H
 
+// Include necessary headers
+#include "Date.h"
+#include "Job.h"
+#include "Candidate.h"
+#include <string>
+#include <iostream>
 
 class Apply {
+private:
+
+    long numberOfSubmission;
+    long candidateID;
+    long jobID;
+    Job* job;
+    Candidate* candidate;
+    Date submissionDate;
+    string submissionStatus;
+
+public:
+
+    //constructors:
+    Apply(); //Default constructor
+    Apply(long numberOfSubmission, long candidateID, long jobID,Job* job,
+          Candidate* candidate, Date submissionDate, string submissionStatus); //Parameter constructor
+    Apply(const Apply& other);  //Copy constructor
+
+
+    //set functions:
+    void setNumberOfSubmission(long numberOfSubmission);
+    void setCandidateID(long candidateID);
+    void setJobID(long jobID);
+    void setJob(Job* job);
+    void setCandidate( Candidate* candidate);
+    void setSubmissionDate(const Date& submissionDate);
+    void setSubmissionStatus(const string& submissionStatus);
+
+
+    //get functions:
+    long getNumberOfSubmission() const;
+    long getCandidateID() const;
+    long getJobID() const;
+    const Job* getJob() const;
+    const Candidate* getCandidate() const;
+    Date getSubmissionDate() const;
+    string getSubmissionStatus() const;
+
+    //other functions:
+    bool operator==(const Apply& other) const; // == operator
+
+    // Print functions:
+    void printEmployer() const;
+    void printCandidate() const;
+
+    //destructor:
+    ~Apply();
 
 };
 
