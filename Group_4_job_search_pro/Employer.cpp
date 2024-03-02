060423e3aa76545ecf525dcced883d901bf4d65f
gadHasson6 <gadi.hasson6@gmail.com>
2024-03-03 00:39:39 +0200

    update after fixing link problems and circular inclusion #4

--- Group_4_job_search_pro/Employer.cpp
+++ Group_4_job_search_pro/Employer.cpp
@@ -42,118 +42,66 @@ void Employer::Edit_Job() {
     bool flag = false;
     for (int i = 0; i < num_of_jobs; i++) {
         if (my_job[i]->get_job_id() == numofjob) {
-            cout
-                    << "tap 1 for edit your company name, 2 for edit your requirements, 3 for edit your location, 4 for edit your occupation, 5 for edit your job type, 6 for your experience, 7 for your contact number, 8 for cancel \n";
+            cout<< "tap 1 for edit your company name, 2 for edit your requirements, 3 for edit your location, 4 for edit your occupation, 5 for edit your job type, 6 for your experience, 7 for your contact number, 8 for cancel \n";
+            cin>>num;
             switch (num) {
                 case 1: {
-                    bool flag2 = true;
-                    while (flag2){
+                    bool flag2= true;
+                    while (flag2) {
                         string newName;
                         cout << "Please enter the new company name: " << endl;
                         cin >> newName;
-                        flag2 = my_job[i]->set_name(newName);
+                        flag2=my_job[i]->set_name(newName);
                     }
-
                 }
                 case 2: {
-                    string newRequirements;
-                    cout << "Please enter the new requirements : " << endl;
-                    cin >> newRequirements;
-                    my_job[i]->set_requirements(newRequirements);
+                    bool flag2= true;
+                    while (flag2) {
+                        string newRequirements;
+                        cout << "Please enter the new requirements : " << endl;
+                        cin >> newRequirements;
+                        flag2=my_job[i]->set_requirements(newRequirements);
+                    }
                 }
                 case 3: {
-                    string newLocation;
-                    int choice=0;
-                    cout<<"please choose your residence: "<<endl;
-                    cout<<"1. Northern District"<<endl;
-                    cout<<"2. Haifa District"<<endl;
-                    cout<<"3. Tel Aviv District"<<endl;
-                    cout<<"4. Central District"<<endl;
-                    cout<<"5. Jerusalem District"<<endl;
-                    cout<<"6. Southern District"<<endl;
-                    cin>>choice;
-                    if (!cin.fail() && choice >= 1 && choice <= 6){
-                        switch (choice) {
-                            case 1:{
-                                newLocation="Northern District";
-                                my_job[i]->set_location(newLocation);
-                                break;
-                            }
-                            case 2: {
-                                newLocation="Haifa District";
-                                my_job[i]->set_location(newLocation);
-                                break;
-                            }
-                            case 3: {
-                                newLocation="Tel Aviv District";
-                                my_job[i]->set_location(newLocation);
-                                break;
-                            }
-                            case 4: {
-                                newLocation="Central District";
-                                my_job[i]->set_location(newLocation);
-                                break;
-                            }
-                            case 5: {
-                                newLocation="Jerusalem District";
-                                my_job[i]->set_location(newLocation);
-                                break;
-                            }
-                            case 6: {
-                                newLocation="Southern District";
-                                my_job[i]->set_location(newLocation);
-                                break;
-                            }
-                        }
-                    }
-                    else {
-                        // Clear the error flag
-                        cin.clear();
-                        // Discard invalid input from the input buffer
-                        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    bool flag2= true;
+                    while (flag2) {
+                        flag2=my_job[i]->set_location();
                     }
                 }
                 case 4: {
-                    string newOccupation;
-                    cout << "Please enter the new occupation : " << endl;
-                    cin >> newOccupation;
-                    if (cin.fail()) {
-                        cin.clear(); // Clear the error flag
-                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
-                        cout << "Invalid input. Please enter a valid integer." << endl;
-                        break;
-                    }my_job[i]->set_occupation(newOccupation);
+                    bool flag2= true;
+                    while (flag2) {
+                        string newOccupation;
+                        cout << "Please enter the new occupation : " << endl;
+                        cin >> newOccupation;
+                        flag2=my_job[i]->set_occupation(newOccupation);
+                    }
                 }
                 case 5: {
-                    int newJobType;
-                    cout << "Please enter for full time job 0 and for part time job 1 : " << endl;
-                    cin >> newJobType;
-                    if (cin.fail()) {
-                        cin.clear(); // Clear the error flag
-                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
-                        cout << "Invalid input. Please enter a valid integer." << endl;
-                        break;
+                    bool flag2= true;
+                    while (flag2) {
+                        int newJobType;
+                        cout << "Please enter for full time job 0 and for part time job 1 : " << endl;
+                        cin >> newJobType;
+                        auto x = Job_scope(newJobType);
+                        flag2 = my_job[i]->set_scope(x);
                     }
-                    auto x = Job_scope(newJobType);
-                    my_job[i]->set_scope(x);
                 }
                 case 6: {
-                    int newExperience;
-                    cout << "Please enter the new experience : " << endl;
-                    cin >> newExperience;
-                    if (cin.fail()) {
-                        cin.clear(); // Clear the error flag
-                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
-                        cout << "Invalid input. Please enter a valid integer." << endl;
-                        break;
+                    bool flag2 = true;
+                    while (flag2) {
+                        flag2=my_job[i]->set_experience();
                     }
-                    my_job[i]->set_experience(newExperience);
                 }
                 case 7: {
-                    string newContactNumber;
-                    cout << "Please enter the new contact number : " << endl;
-                    cin >> newContactNumber;
-                    my_job[i]->set_number(newContactNumber);
+                    bool flag2= true;
+                    while (flag2) {
+                        string newContactNumber;
+                        cout << "Please enter the new contact number : " << endl;
+                        cin >> newContactNumber;
+                        flag2=my_job[i]->set_number(newContactNumber);
+                    }
                 }
                 case 8: {
                     cout << "cancel successfully\n";
