76874eed19cbcb4107c7bdc23ab6bbe1a87bd0bf
keojeN <vladir9@gmail.com>
2024-03-02 22:36:20 +0200

    Update #5

--- Group_4_job_search_pro/Job.cpp
+++ Group_4_job_search_pro/Job.cpp
@@ -12,10 +12,11 @@ Job::Job() {
     m_copmany_location="Unknown";
     m_phone_number="Unknown";
     m_requirements="Unknown";
+    m_job_name="Unknown";
     m_scope=0;
     m_experience_needed=0;
 }
-Job::Job(const int& job_id ,const string& occuption,const string& company_name,const string& company_location,const string& company_number,const string& requirements,const Job_scope& scope ,const int& experience){
+Job::Job(const string& name,const int& job_id ,const string& occuption,const string& company_name,const string& company_location,const string& company_number,const string& requirements,const Job_scope& scope ,const int& experience){
     m_job_id=job_id;
     m_occupation=occuption;
     m_company_name=company_name;
@@ -24,21 +25,21 @@ Job::Job(const int& job_id ,const string& occuption,const string& company_name,c
     m_requirements=requirements;
     m_scope=scope;
     m_experience_needed=experience;
+    m_job_name=name;
 }
 
 void Job::print_job() const{
-
     cout << "Job id: " << m_job_id << endl;
-  cout << "Jobs Occupation: " << m_occupation << endl;
-  cout << "Company name: " << m_company_name << endl;
-  cout << "Company location: " << m_copmany_location << endl;
-  cout << "Company phone number: " << m_phone_number << endl;
-  if (m_scope==0)
-      cout << "Full time job" << endl;
-  else
-      cout << "Part time job" << endl;
-  cout << "Requirements: " << m_requirements << endl;
-  cout << m_experience_needed << " of experience needed!" << endl;
+    cout << "Jobs Occupation: " << m_occupation << endl;
+    cout << "Company name: " << m_company_name << endl;
+    cout << "Company location: " << m_copmany_location << endl;
+    cout << "Company phone number: " << m_phone_number << endl;
+    if (m_scope==0)
+        cout << "Full time job" << endl;
+    else
+        cout << "Part time job" << endl;
+    cout << "Requirements: " << m_requirements << endl;
+    cout << m_experience_needed << " of experience needed!" << endl;
 }
 void Job::set_applies(Apply** appliesArray, int size) {
     // Release previously allocated memory
@@ -83,4 +84,148 @@ void Job::Add_Apply(Apply* apply) {
     m_Applies = temp;
 
     cout << "Apply added successfully" << endl;
+}
+bool Job::set_status(const bool status){
+    if(status == 1 || status == 0)
+    {
+        m_posting_status=status;
+        return true;
+    }
+    else
+    {
+        cout << "Status can be only active or inactive " << endl;
+        return false;
+    }
+}
+bool Job::set_occupation(const string& occupation) {
+    if(Checkdigit(occupation))
+    {
+        m_occupation=occupation;
+        return true;
+    }
+    else
+        return false;
+}
+bool Job::set_job_name(const string& job_name){
+    if(Checkdigit(occupation))
+    {
+        m_occupation=occupation;
+        return true;
+    }
+    else
+        return false;
+}
+bool Job::set_name(const string& name) {
+    if(Checkdigit(occupation))
+    {
+        m_occupation=occupation;
+        return true;
+    }
+    else
+        return false;
+}
+bool Job::set_location() {
+    cout<<"please choose your residence: "<<endl;
+    int choice=0;
+    cout<<"1. Northern District"<<endl;
+    cout<<"2. Haifa District"<<endl;
+    cout<<"3. Tel Aviv District"<<endl;
+    cout<<"4. Central District"<<endl;
+    cout<<"5. Jerusalem District"<<endl;
+    cout<<"6. Southern District"<<endl;
+    cin>>choice;
+    if (!cin.fail() && choice >= 1 && choice <= 6){
+        switch (choice) {
+            case 1:{
+                this->m_copmany_location="Northern District";
+                break;
+            }
+            case 2: {
+                this->m_copmany_location="Haifa District";
+                break;
+            }
+            case 3: {
+                this->m_copmany_location="Tel Aviv District";
+                break;
+            }
+            case 4: {
+                this->m_copmany_location="Central District";
+                break;
+            }
+            case 5: {
+                this->m_copmany_location="Jerusalem District";
+                break;
+            }
+            case 6: {
+                this->m_copmany_location="Southern District";
+                break;
+            }
+        }
+        return true;
+    }
+    else {
+        // Clear the error flag
+        cin.clear();
+        // Discard invalid input from the input buffer
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return false;
+    }
+}
+bool Job::set_number(const string& number) {
+    if(isExactly10Digits(number)){
+        m_phone_number = number;
+        return true;
+    }
+    else
+        cout << "Invalid phone number" << endl;
+}
+bool Job::set_requirements(const string& requirements) {
+    m_requirements = requirements;
+}
+
+bool Job::set_scope(const Job_scope& scope) {
+    if(scope==1 || scope == 0)
+    {
+        m_scope = scope;
+        return true;
+    }
+    else
+        cout << "Invalid input" << endl;
+}
+bool Job::set_experience() {
+    int number;
+    cin >> number;
+    if (cin.fail()) {
+        cout << "Invalid input. Please enter an integer." << std::endl;
+        cin.clear(); // Clear the fail state flag
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
+        return false;
+    } else {
+        m_experience_needed=experience;
+        return true;
+    }
+}
+
+bool Job::isExactly10Digits(const std::string& str) {
+    int digitCount = 0;
+    for (char ch : str) {
+        if (isdigit(ch)) {
+            digitCount++;
+        } else {
+            // If a non-digit character is encountered, return false
+            return false;
+        }
+    }
+    // Return true only if exactly 10 digits are encountered
+    return (digitCount == 10);
+}
+
+bool Job::Checkdigit(const string& str){
+    for (char ch : occupation) {
+        if (isdigit(ch)) {
+            cout << "Only letters are allowed " << endl;
+            return false;
+        }
+    }
+    return true;
 }
--- Group_4_job_search_pro/Job.h
+++ Group_4_job_search_pro/Job.h
@@ -4,6 +4,7 @@
 #include <cstring>
 #include "iostream"
 #include "Apply.h"
+#include <cctype>
 #ifndef GROUP_4_JOB_SEARCH_PRO_JOB_H
 #define GROUP_4_JOB_SEARCH_PRO_JOB_H
 enum Job_scope {full_time = 0,part_time};
@@ -17,25 +18,28 @@ public:
     Job(const Job& other) : m_job_id(other.m_job_id) , m_occupation(other.m_occupation) ,m_company_name(other.m_company_name) ,m_copmany_location(other.m_copmany_location) , m_phone_number(other.m_phone_number),
                             m_requirements(other.m_requirements) , m_scope(other.m_scope) , m_experience_needed(other.m_experience_needed) {};
     void print_job () const;
-    void set_occupation(const string& occupation) { m_occupation = occupation; }
-    void set_name(const string& name) { m_company_name = name; }
-    void set_location(const string& location) { m_copmany_location = location; }
-    void set_number(const string& number) { m_phone_number = number; }
-    void set_requirements(const string& requirements) { m_requirements = requirements; }
-    void set_scope(const Job_scope& scope) { m_scope = scope; }
-    void set_experience(const int& experience) { m_experience_needed = experience; }
+    bool set_occupation(const string& occupation);
+    bool set_name(const string& name);
+    bool set_location() ;
+    bool set_number(const string& number);
+    bool set_requirements(const string& requirements);
+    bool set_scope(const Job_scope& scope);
+    bool set_experience();
+    bool set_job_name(const string& job_name);
     void set_applies(Apply** , int );
-    void set_status(const bool status) { m_posting_status=status; }
+    bool set_status(const bool status);
     const string& get_occupation() const { return m_occupation; }
     const string& get_name() const { return m_company_name; }
     const string& get_number() const { return m_phone_number; }
     const string& get_location() const { return m_copmany_location; }
     const string& get_requirements() const { return m_requirements; }
+    const string& get_job_name() const { return m_job_name; }
     const Job_scope& get_scope() const { return m_scope; }
     const int& get_experience() const { return m_experience_needed; }
     const int& get_job_id() const { return m_job_id; }
     const bool get_status () const {return m_posting_status; }
-
+    bool isExactly10Digits(const string& );
+    bool Checkdigit(const string& );
 private:
     bool m_posting_status;
     int m_job_id;
@@ -44,6 +48,7 @@ private:
     string m_copmany_location;
     string m_phone_number;
     string m_requirements;
+    string m_job_name;
     int m_experience_needed;
     Job_scope m_scope;
     m_Apply** m_Applies;
